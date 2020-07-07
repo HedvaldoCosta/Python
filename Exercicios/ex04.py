358a853060aa37cca1fb5983c7c94005748e282d
Khalhkop <67663958+HedvaldoCosta@users.noreply.github.com>
2020-07-07 20:47:52 -0300

    Update and rename ex12.py to ex04.py

---
+++ Exercicios/ex04.py
@@ -0,0 +1,4 @@
+#4-Dissecando uma variavel
+a = input("Me diga algo")
+print("É numerico?", a.isnumeric())
+print("É alfabetico?", a.isalpha())
--- Exercicios/ex12.py
+++
@@ -1,4 +0,0 @@
-#12-Desconto
-preco = float(input("Preço do produto:"))
-desc = preco - (0.05 * preco)
-print(f"Preço com o desconto: {desc}")

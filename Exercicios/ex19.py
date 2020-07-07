99e9ae2c395a26346c29372a87aeb4579b8d32ca
Khalhkop <67663958+HedvaldoCosta@users.noreply.github.com>
2020-07-07 20:39:45 -0300

    Delete ex19.py

--- Exercicios/ex19.py
+++
@@ -1,8 +0,0 @@
-#19-Sorteando um aluno
-import random
-alu1 = str(input("Nome do primeiro aluno:"))
-alu2 = str(input("Nome do segundo aluno:"))
-alu3 = str(input("Nome do terceiro aluno:"))
-alu4 = str(input("Nome do quarto aluno:"))
-aleat = [alu1, alu2, alu3, alu4]
-print(f"O aluno escolhido foi: {random.choice(aleat)}")

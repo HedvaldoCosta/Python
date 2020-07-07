69617183a0d7cacb0fc05eac66a2188d40729b1c
Hedvaldo Costa <hedvaldocostaV.O@hotmail.com>
2020-07-06 23:23:33 -0300

    Atualizando

---
+++ Exercicios/ex1..8.py
@@ -0,0 +1,42 @@
+#Codigo para escrever uma saudação
+print("Ola, mundo")
+
+#Codigo para dar boas vindas ao usuario
+nome = input("Como você se chama?")
+print(f"{nome}, seja muito bem-vindo")
+
+#Codigo para somar dois numeros
+numero1 = int(input("Me diga um numero:"))
+numero2 = int(input("Me diga outro:"))
+soma = (numero1 + numero2)
+print(f"A soma entre {numero1} e {numero2} é igual a {soma}")
+
+#Dissecando uma variavel
+a = input("Me diga algo")
+print("É numerico?", a.isnumeric())
+print("É alfabetico?", a.isalpha())
+#Antecessor e sucessor
+num = int(input("Me diga um numero:"))
+ant = num - 1
+suc = num + 1
+print(f"Antecessor:{ant} e sucessor:{suc}")
+
+#Codigo para ler o dobro, triplo e a raiz de um numero
+numero = int(input("Me diga um numero:"))
+dobro = numero * 2
+triplo = numero * 3
+raiz = numero**(1/2)
+print(f"dobro:{dobro}, triplo:{triplo} e raiz:{raiz}")
+
+#Media de um aluno
+nota = float(input("Me diga a primeira nota:"))
+nota2 = float(input("Me diga a segunda nota:"))
+media = (nota + nota2)/2
+print(f"Media: {media}")
+
+#Conversor de medidas
+medida = float(input("Me diga um valor em metros:"))
+cen = medida * 100
+mil = medida * 1000
+print(f"centimetros:{cen} e milimetros:{mil}")
+

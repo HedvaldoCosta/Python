d9fc4c6096b42d3ede1fc525237599298e29f59d
Khalhkop <67663958+HedvaldoCosta@users.noreply.github.com>
2020-07-07 20:40:10 -0300

    Delete ex3.py

--- Exercicios/ex3.py
+++
@@ -1,5 +0,0 @@
-#3-Codigo para somar dois numeros
-numero1 = int(input("Me diga um numero:"))
-numero2 = int(input("Me diga outro:"))
-soma = (numero1 + numero2)
-print(f"A soma entre {numero1} e {numero2} é igual a {soma}")

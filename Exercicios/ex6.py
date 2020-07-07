71095e2a1d77ddae1da84a47f1df290f2e615652
Khalhkop <67663958+HedvaldoCosta@users.noreply.github.com>
2020-07-07 20:40:33 -0300

    Delete ex6.py

--- Exercicios/ex6.py
+++
@@ -1,6 +0,0 @@
-#6-Codigo para ler o dobro, triplo e a raiz de um numero
-numero = int(input("Me diga um numero:"))
-dobro = numero * 2
-triplo = numero * 3
-raiz = numero**(1/2)
-print(f"dobro:{dobro}, triplo:{triplo} e raiz:{raiz}")

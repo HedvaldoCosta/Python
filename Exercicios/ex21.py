b9b8a0852b7345b7af0fc2665d3064ad734ed456
Hedvaldo Costa <hedvaldocostaV.O@hotmail.com>
2020-07-08 17:39:49 -0300

    Atualizando

---
+++ Exercicios/ex21.py
@@ -0,0 +1,9 @@
+#Tocando musica
+import pygame
+
+# Inicializando o mixer PyGame
+pygame.mixer.init()
+pygame.init()
+pygame.mixer.music.load('ex21.mp3')
+pygame.mixer.music.play()
+pygame.event.wait()
---
+++ Exercicios/ex22.py
@@ -0,0 +1,5 @@
+nome = str(input("Qual é o seu nome? ")).strip()
+print(f"Nome em maiusculas:{nome.upper()}")
+print(f"Nome em minusculas:{nome.lower()}")
+print(f"Letras ao todo:{len(nome) - nome.count(' ')}")
+print(f"Seu primeiro nome tem {nome.find(' ')} letras")

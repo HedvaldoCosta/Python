6fb75294a0a7ca66f297c5b1dc9dd0e95bd4953b
Hedvaldo Costa <hedvaldocostaV.O@hotmail.com>
2020-07-09 10:47:18 -0300

    Atualizando lista

---
+++ Exercicios/ex27.py
@@ -0,0 +1,6 @@
+#Falar o primeiro e o ultimo nome do usuario
+nome = str(input('Qual é o seu nome? ')).strip()
+n = nome.split()
+
+print('Seu primeiro nome é: {}'.format(n[0]))
+print('Seu ultimo nome é: {}'.format(n[len(n)-1]))

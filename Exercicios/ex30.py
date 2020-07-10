c4424021cb2d9c7096aba757cf95f3053f8d8e4a
Hedvaldo Costa <hedvaldocostaV.O@hotmail.com>
2020-07-09 21:07:55 -0300

    Atualizando

---
+++ Exercicios/ex30.py
@@ -0,0 +1,8 @@
+#Ler se o numero é par ou impar
+numero = int(input('Me fale um numero:'))
+num = numero % 2
+
+if num == 0:
+    print('Numero par')
+else:
+    print('Numero impar')
---
+++ Exercicios/ex31.py
@@ -0,0 +1,6 @@
+#Calculando o preço da viagem
+dist = float(input('Viagem de quantos KM? '))
+if dist <= 200:
+    print(f'preço a ser pago: {dist * 0.5}')
+else:
+    print(f'Valor a ser pago: {dist * 0.45}')

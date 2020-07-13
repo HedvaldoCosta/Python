20f64224ac856615885325bc7a0e37f6703a0e55
Hedvaldo Costa <hedvaldocostaV.O@hotmail.com>
2020-07-13 12:34:26 -0300

    Update ex36.py

--- Exercicios/ex36.py
+++ Exercicios/ex36.py
@@ -2,17 +2,10 @@
 salario = float(input('Seu salario:'))
 valor = float(input('Valor da casa:'))
 anos = int(input('Em quantos anos deseja pagar?'))
-mes = anos * 12
-sal = (0.3 * salario)
-pagar = salario * mes
-dif = valor - pagar
+pres = valor/(anos * 12)
+mini = 0.3 * salario
 
-if dif > 0:
-    print('Você não irá conseguir comprar essa casa nesse tempo')
-    print(f'Falta:{valor - dif}')
-elif (sal * mes) < valor:
-    print('Emprestimo negado')
-elif (sal * mes) >= valor:
-    print('Emprestimo aceito')
-
-
+if pres <= mini:
+    print('Emprestimo APROVADO')
+else:
+    print('Emprestimo NEGADO')

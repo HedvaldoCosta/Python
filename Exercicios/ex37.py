da736a3eedf76f092a63b0a2f3497613419ab30b
Hedvaldo Costa <hedvaldocostaV.O@hotmail.com>
2020-07-13 12:58:19 -0300

    Update ex37.py

--- Exercicios/ex37.py
+++ Exercicios/ex37.py
@@ -6,8 +6,10 @@ resp = int(input("""[1] para binario
 """))
 
 if resp == 1:
-    print(bin(numero))
+    print(bin(numero)[2:])
 elif resp == 2:
-    print(oct(numero))
+    print(oct(numero)[2:])
 elif resp == 3:
-    print(hex(numero))
+    print(hex(numero)[2:])
+else:
+    print('\033[31mCOMANDO INVALIDO')

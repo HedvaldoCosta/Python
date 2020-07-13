412be697046c5e79783589a907dc69180cf7a662
Hedvaldo Costa <hedvaldocostaV.O@hotmail.com>
2020-07-12 23:09:15 -0300

    Create ex41.py

---
+++ Exercicios/ex41.py
@@ -0,0 +1,15 @@
+#Classificando atletas
+idade = int(input('Sua Idade:'))
+
+if idade <= 9 and idade > 0:
+    print('MIRIM')
+elif idade > 9 and idade <=14 and idade > 0:
+    print('INFANTIL')
+elif idade > 0 and idade > 14 and idade <= 19:
+    print('JUNIOR')
+elif idade == 20 and idade > 0:
+    print('SÊNIOR')
+elif idade > 20 and idade > 0:
+    print('MASTER')
+if idade < 0:
+    print('\033[4;31mHAHA CLASSIC!!!')

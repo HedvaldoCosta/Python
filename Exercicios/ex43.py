7ae9cb7bd2653cd0000d31c2cc87596f2324eced
Hedvaldo Costa <hedvaldocostaV.O@hotmail.com>
2020-07-13 18:03:39 -0300

    Create ex43.py

---
+++ Exercicios/ex43.py
@@ -0,0 +1,16 @@
+#IMC
+altura = float(input('Sua altura: '))
+peso = float(input('Seu peso:'))
+imc = peso/(altura**2)
+
+if imc < 18.5:
+    print(('ABAIXO DO PESO'))
+elif imc > 18.5 and imc < 25:
+    print('PESO IDEAL')
+elif imc >= 25 and imc < 30:
+    print('SOBRE PESO')
+elif imc >= 30 and imc < 40:
+    print('OBESIDADE')
+else:
+    print('OBESIDADE MORBIDA')
+print(f'O IMC dessa pessoa é {imc:.2f}')

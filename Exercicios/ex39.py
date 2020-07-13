8cd7112e845846ba133e73aa4e18acd07eae1bd2
Hedvaldo Costa <hedvaldocostaV.O@hotmail.com>
2020-07-13 13:16:30 -0300

    Update ex39.py

--- Exercicios/ex39.py
+++ Exercicios/ex39.py
@@ -1,14 +1,17 @@
 #Alistamento
 from datetime import date
 idade = int(input('Ano de nascimento:'))
+sexo = str(input('Genero(M/F):')).upper()
 ano = date.today().year
 alis = ano - idade
 pas = alis - 18
 che = 18 - alis
 
-if alis == 18:
+if sexo == 'M' and alis == 18:
     print('Esta na hora de se alistar')
-elif alis > 18:
+elif sexo == 'M' and alis > 18:
     print(f'Ja passou {pas} anos para se alistar')
-elif alis < 18:
+elif sexo == 'M' and alis < 18:
     print(f'Ainda falta {che} anos para se alistar')
+elif sexo =='F':
+    print('Não precisa fazer o alistamento')

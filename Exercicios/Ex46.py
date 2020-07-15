00b5fe334566d31c8fd7a678c29c09f548428d41
Hedvaldo Costa <hedvaldocostaV.O@hotmail.com>
2020-07-15 10:51:29 -0300

    Adicionando
    Contagem regressiva de 10 ate 0 para queima de fogos

---
+++ Exercicios/Ex46.py
@@ -0,0 +1,16 @@
+# Contagem regressiva
+import pygame
+from time import sleep
+for c in range(10, -1, -1):
+    sleep(1)
+    print(c)
+print("\033[31m CONTAGEM FINALIZADA")
+pygame.mixer.init()
+pygame.init()
+pygame.mixer.music.load('ex46.mp3')
+pygame.mixer.music.play()
+pygame.event.wait()
+
+
+
+

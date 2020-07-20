94c6a40a10240cf1a82acfbb91de9a11d0149834
Hedvaldo Costa <hedvaldocostaV.O@hotmail.com>
2020-07-20 19:38:09 -0300

    Create Ex60v2.py

---
+++ Exercicios/Ex60v2.py
@@ -0,0 +1,9 @@
+# Usando for
+f = 1
+n = int(input('Me diga um numero: '))
+print(f'{n}! = ')
+for c in range(n, 0, -1):
+    f = f * c
+    print(f'{c}', end=' ')
+    print('X' if c > 1 else ' = ', end=' ')
+print(f)

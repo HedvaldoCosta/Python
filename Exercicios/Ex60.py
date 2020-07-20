0dbeea69fd1e322a3b3c999bafc6a1e6fb70afc4
Hedvaldo Costa <hedvaldocostaV.O@hotmail.com>
2020-07-20 19:29:40 -0300

    Create Ex60.py

---
+++ Exercicios/Ex60.py
@@ -0,0 +1,13 @@
+# fatorial
+num = int(input('Diga-me um numero: '))
+cont = num
+mult = 1
+print(f'{num}! = ', end=' ')
+while cont > 0:
+    print(f'{cont}', end= ' ')
+    print(' X 'if cont > 1 else ' = ', end=' ')
+    mult = mult * cont
+    cont = cont - 1
+
+print(mult)
+

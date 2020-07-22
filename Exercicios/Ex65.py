d0290d41014c1aa0cf79119b9aca2ba7654ad615
Hedvaldo Costa <hedvaldocostaV.O@hotmail.com>
2020-07-22 14:36:36 -0300

    Create Ex65.py

---
+++ Exercicios/Ex65.py
@@ -0,0 +1,19 @@
+# Maior e menor
+soma = c = maior = menor = 0
+resp = 'S'
+while resp == 'S':
+    num = int(input('Numero: '))
+    c= c + 1
+    soma = soma + num
+    if c == 1:
+        maior = num
+        menor = num
+    elif num > maior:
+        maior = num
+    elif num < menor:
+        menor = num
+    resp = str(input('Quer continuar? [S/N]')).upper().strip()[0]
+print(f'Você digitou {c} números e a Média entre eles é: {soma/c}')
+print(f'Maior numero: {maior}')
+print(f'Menor numero: {menor}')
+

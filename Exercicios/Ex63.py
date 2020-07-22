69fff35f08162817cad9a4536b5cc40aa5738635
Hedvaldo Costa <hedvaldocostaV.O@hotmail.com>
2020-07-22 13:54:05 -0300

    Create Ex63.py

---
+++ Exercicios/Ex63.py
@@ -0,0 +1,19 @@
+#Fibonacci
+termos = int(input('Quantos termos: '))
+t = 0
+t1 = 1
+resp = 0
+print(f'{t} -> {t1} ->', end=' ')
+c = 3
+while resp != -1:
+    termos = termos + resp
+    while c <= termos:
+
+        t2 = t + t1
+        print(f'{t2} ->', end=' ')
+        c = c + 1
+        t = t1
+        t1 = t2
+    print('...')
+    resp = int(input('Mais termos(-1 para sair): '))
+

cf13ff695611332b22826fb4751ace53b14dfd3f
Hedvaldo Costa <hedvaldocostaV.O@hotmail.com>
2020-07-26 14:07:51 -0300

    Create Ex66.py

---
+++ Exercicios/Ex66.py
@@ -0,0 +1,9 @@
+s = c = 0
+while True:
+    num = int(input('Me diga um numero[999 para sair]: '))
+    if num == 999:
+        break
+    s = s + num
+    c = c + 1
+print(f'Soma entre os numeros:{s}')
+print(f'Numeros digitados: {c}')

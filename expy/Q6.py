65597799d5b305ba189f5d7b089c981464ee2841
Hedvaldo Costa <hedvaldocostaV.O@hotmail.com>
2020-07-27 22:06:13 -0300

    Create Q6.py

---
+++ expy/Q6.py
@@ -0,0 +1,9 @@
+# Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro.
+# Depois modifique o programa para que ele mostre os números um ao lado do outro.
+for c in range(1, 21):
+    print(c)
+    c = c + 1
+    
+for cont in range(1, 21):
+    print(cont, end=' ')
+    cont = cont + 1

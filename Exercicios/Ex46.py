2439d1072f8d9dc682939870e8f896fed1d91b9c
Hedvaldo Costa <hedvaldocostaV.O@hotmail.com>
2020-07-27 22:24:18 -0300

    Update Ex46.py

--- Exercicios/Ex46.py
+++ Exercicios/Ex46.py
@@ -4,7 +4,7 @@ from time import sleep
 for c in range(10, -1, -1):
     sleep(1)
     print(c)
-print("\033[31m CONTAGEM FINALIZADA")
+print("\033[30m CONTAGEM FINALIZADA")
 pygame.mixer.init()
 pygame.init()
 pygame.mixer.music.load('ex46.mp3')

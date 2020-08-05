b1e1deb6b272b74f917de7695e8a9031b57cb14b
Hedvaldo Costa <hedvaldocostaV.O@hotmail.com>
2020-08-05 13:41:07 -0300

    Create Ex77.py

---
+++ Exercicios/Ex77.py
@@ -0,0 +1,9 @@
+# Crie um programa que tenha uma tupla com várias palavras (não usar acentos). Depois disso, você deve mostrar,
+# para cada palavra, quais são as suas vogais.
+
+palavras = ('Aurora Boreal', 'Semana', 'Vegetal', 'Parceria', 'Funil', 'Kiwi')
+for c in palavras:
+    print(f'\nNa palavra \033[31m{c}\033[m temos: ', end='')
+    for vogal in c:
+        if vogal.lower() in 'aeiou':
+            print(f'{vogal}', end=' ')

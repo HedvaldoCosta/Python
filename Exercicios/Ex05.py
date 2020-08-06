545c1a20d4fff975ba9254ffe3ad1e4280a732af
Khalhkop <67663958+HedvaldoCosta@users.noreply.github.com>
2020-08-06 14:43:40 -0300

    Update and rename Ex50.py to Ex05.py

---
+++ Exercicios/Ex05.py
@@ -0,0 +1,5 @@
+#5-Antecessor e sucessor
+num = int(input("Me diga um numero:"))
+ant = num - 1
+suc = num + 1
+print(f"Antecessor:\033[33m{ant}\033[m e sucessor:\033[32m{suc}\033[m")
--- Exercicios/Ex50.py
+++
@@ -1,8 +0,0 @@
-# Soma dos Pares
-s = 0
-for c in range(1, 7):
-    num = int(input(f"Diga-me o {c}º numero:"))
-    if num % 2 == 0:
-        s = s + num
-print(f"Soma dos pares:{s}")
-

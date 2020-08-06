51121a49b52056ed03ac8d2c1f1f9978ec3d7534
Khalhkop <67663958+HedvaldoCosta@users.noreply.github.com>
2020-08-06 14:46:11 -0300

    Update and rename Ex52.py to Ex07.py

---
+++ Exercicios/Ex07.py
@@ -0,0 +1,5 @@
+#7-Media de um aluno
+nota = float(input("Me diga a primeira nota:"))
+nota2 = float(input("Me diga a segunda nota:"))
+media = (nota + nota2)/2
+print(f"Media: \033[3;30m{media}\033[m")

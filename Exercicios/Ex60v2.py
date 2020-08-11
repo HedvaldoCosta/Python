433ff1bf083986345a48d2c6ae44998f6937d6e0
Khalhkop <67663958+HedvaldoCosta@users.noreply.github.com>
2020-08-11 18:59:29 -0300

    Update and rename Ex60v2.py to Ex16.py

--- Exercicios/Ex60v2.py
+++
@@ -1,9 +0,0 @@
-# Usando for
-f = 1
-n = int(input('Me diga um numero: '))
-print(f'{n}! = ', end=' ')
-for c in range(n, 0, -1):
-    f = f * c
-    print(f'{c}', end=' ')
-    print('X' if c > 1 else ' = ', end=' ')
-print(f)

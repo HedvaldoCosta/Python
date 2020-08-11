a68b47bd8d7bf48a07f1b1041e589d55d970f53d
Khalhkop <67663958+HedvaldoCosta@users.noreply.github.com>
2020-08-11 18:59:56 -0300

    Update and rename Ex61.py to Ex17.py

--- Exercicios/Ex61.py
+++
@@ -1,8 +0,0 @@
-# PA
-termo = float(input('Primeiro termo: '))
-razão = float(input('Razão entre eles: '))
-c = 1
-while c <= 10:
-    print(f'{c}º termo:{termo}')
-    c = c + 1
-    termo = termo + razão

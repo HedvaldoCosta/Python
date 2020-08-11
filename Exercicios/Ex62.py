0db518eaa13989f2fee2fca56488a87372be724f
Khalhkop <67663958+HedvaldoCosta@users.noreply.github.com>
2020-08-11 19:00:20 -0300

    Update Ex62.py

--- Exercicios/Ex62.py
+++ Exercicios/Ex62.py
@@ -1,19 +1,7 @@
-# SUPER PA
-termo = float(input('Me diga um numero: '))
-razão = float(input('Razão entre eles: '))
-c = 1
-total = 0
-resp = 10
-while resp != 0:
-    total = total + resp
-    while c <= total:
-        c = c + 1
-        print(f'{termo} ->', end=' ' )
-        termo = termo + razão
-    print('...')
-    resp = int(input('''Deseja adicionar mais quantos termos?(0 para sair)
-    '''))
-print('Programa finalizado')
-print(f'{total} termos')
-
-
+#18-Cos, sen e tag de um angulo
+import math
+ang = float(input("Angulo:"))
+ang2 = math.radians(ang)
+print(f"Seno de {ang}:{math.sin(ang2):.2f}")
+print(f"Cosseno de {ang}:{math.cos(ang2):.2f}")
+print(f"Tangente de {ang}:{math.tan(ang2):.2f}")

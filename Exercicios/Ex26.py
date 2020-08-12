88d5d3d32d1b4749d0e03dba70b5b6595c0a9f6f
Khalhkop <67663958+HedvaldoCosta@users.noreply.github.com>
2020-08-12 20:19:31 -0300

    Update and rename Ex70.py to Ex26.py

---
+++ Exercicios/Ex26.py
@@ -0,0 +1,5 @@
+#26-Mostrar quantas vezes a letra A foi escrita e em que posição
+frase = str(input('Digite uma frase:')).strip()
+print('A letra A aparece: {} vezes'.format(frase.upper().count('A')))
+print('A primeira letra A encontrada esta na posição: {}'.format(frase.upper().find('A')+1))
+print('A ultima letra A encontrada esta na posição: {}'.format(frase.upper().rfind('A')+1))

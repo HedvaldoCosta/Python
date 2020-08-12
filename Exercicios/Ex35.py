26796534c6dfa0f401d2363f13b68382e83c845f
Khalhkop <67663958+HedvaldoCosta@users.noreply.github.com>
2020-08-12 20:25:26 -0300

    Update and rename ex02.py to Ex35.py

---
+++ Exercicios/Ex35.py
@@ -0,0 +1,8 @@
+#35-Formação de um triangulo
+r1 = float(input('Segmento 1: '))
+r2 = float(input('Segmento 2: '))
+r3 = float(input('Segmento 3:'))
+if r1 + r2 > r3 and r1 + r3 > r2 and r2 + r3 > r1:
+    print('Pode formar um triangulo!')
+else:
+    print('Não pode formar um triangulo!')

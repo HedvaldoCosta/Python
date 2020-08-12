53509433c3a16bba546a64ed70b107e35196e4c3
Khalhkop <67663958+HedvaldoCosta@users.noreply.github.com>
2020-08-12 20:28:13 -0300

    Update and rename ex05.py to Ex38.py

---
+++ Exercicios/Ex38.py
@@ -0,0 +1,10 @@
+#38-Comparando numeros
+numero = int(input('1º numero:'))
+numero2 = int(input('2º numero'))
+
+if numero > numero2:
+    print('1º numero é maior')
+elif numero < numero2:
+    print('2º numero é maior')
+else:
+    print('Numeros iguais')

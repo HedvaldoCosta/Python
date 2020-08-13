860a72c611dc4e56efba78f3b42ca0d78a9b15c3
Khalhkop <67663958+HedvaldoCosta@users.noreply.github.com>
2020-08-13 20:47:56 -0300

    Update and rename ex16.py to Ex49.py

---
+++ Exercicios/Ex49.py
@@ -0,0 +1,4 @@
+#49-Tabuada V2.0
+num = int(input("Diga-me um número: "))
+for c in range(0, 11):
+    print(f"{num} X {c} = {num * c}")
--- Exercicios/ex16.py
+++
@@ -1,5 +0,0 @@
-#16-Mostrar a forma inteira de um numero real
-import math
-num = float(input("Me diga um numero real:"))
-flo = math.floor(num)
-print(f"Numero inteiro:{flo}")

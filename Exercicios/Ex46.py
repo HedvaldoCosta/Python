ae9011f329b29a4dcb2ef1630d349bcfe2a0327b
Khalhkop <67663958+HedvaldoCosta@users.noreply.github.com>
2020-08-13 20:45:08 -0300

    Update and rename ex13.py to Ex46.py

---
+++ Exercicios/Ex46.py
@@ -0,0 +1,12 @@
+#46-Contagem regressiva
+import pygame
+from time import sleep
+for c in range(10, -1, -1):
+    sleep(1)
+    print(c)
+print("\033[31m CONTAGEM FINALIZADA")
+pygame.mixer.init()
+pygame.init()
+pygame.mixer.music.load('ex46.mp3')
+pygame.mixer.music.play()
+pygame.event.wait()

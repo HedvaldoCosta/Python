5ebc49d61adc624fd6552df1e351fce3596e4610
Khalhkop <67663958+HedvaldoCosta@users.noreply.github.com>
2020-08-13 20:42:07 -0300

    Update and rename ex08.py to Ex41.py

--- Exercicios/ex08.py
+++
@@ -1,5 +0,0 @@
-#8-Conversor de medidas
-medida = float(input("Me diga um valor em metros:"))
-cen = medida * 100
-mil = medida * 1000
-print(f"centimetros:\033[33m{cen}\033[m e milimetros:\033[30m{mil}\033[m")

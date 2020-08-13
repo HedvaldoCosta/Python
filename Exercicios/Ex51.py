627f70485564075023984da04ee4b14142c616d0
Khalhkop <67663958+HedvaldoCosta@users.noreply.github.com>
2020-08-13 20:59:51 -0300

    Update and rename ex18.py to Ex51.py

---
+++ Exercicios/Ex51.py
@@ -0,0 +1,6 @@
+#51-Progressão Aritmetica
+termo = float(input("Primeiro termo: "))
+razão = float(input("Razão:"))
+for c in range(2, 11):
+    termo = termo + razão
+    print(f"{c}º termo: {termo}")
--- Exercicios/ex18.py
+++
@@ -1,7 +0,0 @@
-#18-Cos, sen e tag de um angulo
-import math
-ang = float(input("Angulo:"))
-ang2 = math.radians(ang)
-print(f"Seno de {ang}:{math.sin(ang2):.2f}")
-print(f"Cosseno de {ang}:{math.cos(ang2):.2f}")
-print(f"Tangente de {ang}:{math.tan(ang2):.2f}")

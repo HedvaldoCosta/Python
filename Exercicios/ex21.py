4da8dc15fe828bc40bf6d50e2263e39f7c8d74a1
Khalhkop <67663958+HedvaldoCosta@users.noreply.github.com>
2020-08-13 21:01:25 -0300

    Update and rename ex21.py to Ex54.py

--- Exercicios/ex21.py
+++
@@ -1,9 +0,0 @@
-#Tocando musica
-import pygame
-
-# Inicializando o mixer PyGame
-pygame.mixer.init()
-pygame.init()
-pygame.mixer.music.load('ex21.mp3')
-pygame.mixer.music.play()
-pygame.event.wait()

02e213263cfe82f04f6e140fad300214e5cfc5eb
Khalhkop <67663958+HedvaldoCosta@users.noreply.github.com>
2020-08-14 21:39:30 -0300

    Update and rename ex24.py to Ex57.py

---
+++ Exercicios/Ex57.py
@@ -0,0 +1,9 @@
+#57-Validando dados
+sexo = str(input('Seu sexo(M/F): ')).upper().strip()[0]
+if sexo != 'M, F':
+    while sexo != 'M' and sexo != 'F':
+        sexo = str(input('Dado invalido. Digite seu sexo: ')).upper().strip()[0]
+if sexo == 'M':
+    print('Masculino')
+elif sexo == 'F':
+    print('Feminino')
--- Exercicios/ex24.py
+++
@@ -1,4 +0,0 @@
-#Verificar se 'santo' é o primeiro nome da cidade
-cidade = str(input('Nome da sua cidade:')).strip()
-print(cidade[:5].upper() == 'SANTO')
-

858da91d9f24a3276b1068319454f449b0eb6f6b
Khalhkop <67663958+HedvaldoCosta@users.noreply.github.com>
2020-08-15 22:25:06 -0300

    Update and rename ex35.py to Ex68.py

---
+++ Exercicios/Ex68.py
@@ -0,0 +1,25 @@
+#68-Par ou impar
+from random import randint
+s = c = 0
+while True:
+    valor = int(input('Me diga um valor: '))
+    rand = randint(0, 10)
+    opc = ' '
+    while opc not in 'PI':
+        opc = str(input('[\033[36mP\033[m] para par e [\033[31mI\033[m] para impar: ')).upper().strip()[0]
+    if opc == 'P':
+        s = rand + valor
+        if s % 2 == 0:
+            print(f'Você jogou {valor} e a maquina {rand} dando {s}. \033[32mGANHOU!\033[m')
+        else:
+            print(f'Você jogou {valor} e a maquina {rand} dando {s}. \033[31mPERDEU!\033[m')
+            break
+    if opc == 'I':
+        s = rand + valor
+        if s % 2 == 0:
+            print(f'Você jogou {valor} e a maquina {rand} dando {s}. \033[31mPERDEU!\033[m')
+            break
+        else:
+            print(f'Você jogou {valor} e a maquina {rand} dando {s}. \033[32mGANHOU!\033[m')
+    c = c + 1
+print(f'Numero total de vitorias seguidas: {c}')

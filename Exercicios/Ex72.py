9173059e97716e5f165800d844d60dc5ef660e30
Khalhkop <67663958+HedvaldoCosta@users.noreply.github.com>
2020-08-17 21:58:36 -0300

    Update and rename ex39.py to Ex72.py

---
+++ Exercicios/Ex72.py
@@ -0,0 +1,15 @@
+#72-Crie um programa que tenha uma tupla totalmente preenchida com uma contagem por extenso, de zero até vinte.
+# Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
+
+numero = ('Zero', 'Um', 'dois', 'três', 'Quatro', 'Cinco',
+          'Seis', 'sete', 'Oito', 'nove', 'dez',
+          'Onze', 'Doze', 'treze', 'catorze', 'quinze',
+          'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+while True:
+    num = int(input('Me diga um numero entre 0 e 20: '))
+    while num < 0 or num > 20:
+        print('Numero invalido. Escolha apenas numeros entre 0 e 20')
+        num = int(input('Me diga um numero entre 0 e 20: '))
+    print(numero[num])
+
+

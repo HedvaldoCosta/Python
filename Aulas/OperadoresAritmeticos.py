f92f230970fcd0268f519828b2a707700651be24
Hedvaldo Costa <hedvaldocostaV.O@hotmail.com>
2020-11-20 23:30:02 -0300

    Create OperadoresAritmeticos.py

---
+++ Aulas/OperadoresAritmeticos.py
@@ -0,0 +1,20 @@
+(+) = soma
+5 + 6 = 11
+
+(-) = Subtração
+8 - 3 = 5
+
+(*) = Multiplicação
+5 * 5 = 25
+
+(/) = Divisão
+10 / 2 = 5
+
+(**) = Potência
+8 ** 2 = 64
+
+(//) = Divisão por inteiro
+5 // 2 = 2
+
+(%) = Resto da divisão
+5 // 2 = 1
